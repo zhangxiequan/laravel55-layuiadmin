68e99add5ee97183c9afc7422b8746f3e6e5614a
zxquan <376195259@qq.com>
2019-11-19 17:26:56 +0800

    修改title

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -4,7 +4,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>layuiAdmin std - 通用后台管理模板系统（iframe标准版）</title>
+    <title>彼岸繁华 - 不只是做一名工程师，而是要成为一名工匠！</title>
     <meta name="renderer" content="webkit">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
